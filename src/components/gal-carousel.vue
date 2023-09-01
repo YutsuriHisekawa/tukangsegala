4bbc3e4f553dec70bcc12da2faea01f3f47b9ce4
Hisekawa <tetsuyakumiko01@gmail.com>
2023-09-01 14:52:55 +0700

    Tukang Segala

--- src/components/gal-carousel.vue
+++ src/components/gal-carousel.vue
@@ -1,6 +1,6 @@
 <template>
   <Carousel
-    :items-to-show="2.5"
+    :items-to-show="itemsToShow"
     :wrap-around="true">
     <Slide
       v-for="(img, index) in images"
@@ -10,21 +10,24 @@
           :src="img"
           alt="Slide Image"
           class="w-full object-cover"
-          :style="{ height: '700px' }" />
+          :style="{ height: '440px' }" />
         <div class="absolute top-0 left-0 w-full h-full flex items-center justify-center">
           {{ index + 1 }}
         </div>
       </div>
     </Slide>
 
-    <template #addons>
+    <!-- Hide navigation on medium screens and above -->
+    <template
+      #addons
+      class="md:hidden">
       <Navigation />
     </template>
   </Carousel>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, ref, onMounted } from 'vue';
 import { Carousel, Navigation, Slide } from 'vue3-carousel';
 
 import 'vue3-carousel/dist/carousel.css';
@@ -36,8 +39,18 @@ export default defineComponent({
     Slide,
     Navigation,
   },
-  data() {
+  setup() {
+    const itemsToShow = ref(4); // default for large screens
+
+    onMounted(() => {
+      // Adjust number of items based on window width
+      if (window.innerWidth < 640) itemsToShow.value = 1; // sm
+      else if (window.innerWidth < 768) itemsToShow.value = 2; // md
+      else if (window.innerWidth < 1024) itemsToShow.value = 3; // lg
+    });
+
     return {
+      itemsToShow,
       images: ['/testi/1.png', '/testi/2.png', '/testi/3.png', '/testi/4.png'],
     };
   },
